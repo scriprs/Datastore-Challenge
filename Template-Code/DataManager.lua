be0c069e36fcac388244b6adc916fa471fe9c5be
scriprs <168645319+scriprs@users.noreply.github.com>
2024-05-11 20:39:36 +0200

    DataManager.lua

--- Template-Code/DataManager.lua
+++ Template-Code/DataManager.lua
@@ -1,1 +1,19 @@
+--> DataManager.luau <--
 
+local DataManager = {}
+
+--\\ Functions | Methods //--
+DataManager.load = function(Player: Player, DataStore: DataStore)
+	local key = `Data-{tostring(Player.UserId)}`
+end
+
+DataManager.save = function(Player: Player, DataStore: DataStore)
+	local key =  `Data-{tostring(Player.UserId)}`
+end
+
+DataManager.getData = function(Player: Player, Child: any?)
+	
+end
+
+shared.GetData = DataManager.getData
+return DataManager
