09cfe1c458ffe962c665b36c0a243cc523f8b46c
scriprs <168645319+scriprs@users.noreply.github.com>
2024-05-11 20:43:32 +0200

    DataScript.lua

--- Template-Code/DataScript.lua
+++ Template-Code/DataScript.lua
@@ -1,1 +1,16 @@
 
+--> DataScript.luau <--
+
+--\\ Services //--
+
+--\\ Variables //--
+local dataManager = require(script.DataManager)
+
+--\\ Connections //--
+game.Players.PlayerAdded:Connect(function(player: Player)
+	task.spawn(dataManager.load, player, DataStore)
+end)
+
+game.Players.PlayerRemoving:Connect(function(player: Player)
+  
+end)
