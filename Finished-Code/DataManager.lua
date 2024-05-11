5e5c5fd4b3973bcc2eefda8db808c12405fbf1ad
scriprs <168645319+scriprs@users.noreply.github.com>
2024-05-11 20:37:07 +0200

    DataManager.lua

--- Finished-Code/DataManager.lua
+++ Finished-Code/DataManager.lua
@@ -1,1 +1,54 @@
+--> DataManager.luau <--
 
+local DataManager = {}
+
+--\\ Functions | Methods //--
+DataManager.load = function(Player: Player, DataStore: DataStore)
+	local key = `Data-{tostring(Player.UserId)}`
+	local success, data = pcall(DataStore.GetAsync, DataStore, key)
+
+	if success then
+		local Folder = Instance.new("Folder")
+		Folder.Name = "InternalData"
+
+		local LoginCounter = Instance.new("NumberValue")
+		LoginCounter.Name = "LoginCounter"
+		LoginCounter.Value = if typeof(data) == "number" then data else 0
+
+		LoginCounter.Parent = Folder
+		Folder.Parent = Player
+
+		LoginCounter.Value += 1
+	else
+		return warn("Error loading data")
+	end
+
+	print("loaded")
+end
+
+DataManager.save = function(Player: Player, DataStore: DataStore)
+	local key =  `Data-{tostring(Player.UserId)}`
+	local folder = Player:FindFirstChild("InternalData")
+
+	if folder then
+		local success, data
+		
+		for _, child in ipairs(folder:GetChildren()) do
+			pcall(DataStore.SetAsync, DataStore, key, child.Value)
+		end
+	else
+		warn("InternalData folder not found for player:", Player.Name)
+	end
+
+	print("saved")
+end
+
+DataManager.getData = function(Player: Player, Child: any?)
+	local folder = Player:FindFirstChild("InternalData")
+	
+	if not Child then return folder end
+	return folder[Child]
+end
+
+shared.GetData = DataManager.getData
+return DataManager
